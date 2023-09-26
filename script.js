3bed422267ae780689f8160740e4a741eec4cdbe
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-26 12:28:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+document.addEventListener("domVontentLoaded", ()=>{
+	const para = document.createElement("p");
+	para.innerText = "DOM load success";
+	document.body.appendChild(para);
+})
