aba8368ffa4d06f2a757ea1e3dfda82f97dc66a3
Yash Sharma <139811734+YashSharma412@users.noreply.github.com>
2023-09-28 12:56:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", ()=>{
 	let body = document.body;
-	if (body.chilNodes.length === 0) {
+	// if (body.chilNodes.length == 0) {
 		const para = document.createElement("p");
 		para.innerText = "DOM load success";
 		document.body.appendChild(para);
-	}
+	// }
 })
